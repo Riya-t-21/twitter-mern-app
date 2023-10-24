8932e376d12db8e8c5c7879591f8650fd4a239f6
Riya-t-21 <102308368+Riya-t-21@users.noreply.github.com>
2023-10-24 16:52:17 +0530

    First Commit

---
+++ components/layout/SidebarItem.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {IconType} from  'react-icons';
+
+interface SidebarItemProps {
+    label: string;
+    href: string;
+    icon: IconType;
+    onClick?: () => void;
+}
+
+const SidebarItem: React.FC<SidebarItemProps> = ( { 
+    label,
+    href,
+    icon,
+    onClick
+}) => {
+     return (
+    <div></div>
+  )
+}
+
+export default SidebarItem
